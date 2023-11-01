d99f3ddc55b7693954539cc5f5ed647c8e33ae25
SamuelAdds123 <147607777+SamuelAdds123@users.noreply.github.com>
2023-11-01 20:45:43 +0000

    Added Project to main

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    public bool hasPowerup;
+    private float powerupStrength = 15.0f;
+    public GameObject powerIndicator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        powerIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+            powerIndicator.gameObject.SetActive(true);
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            
+            
+
+            {
+
+
+            }
+
+
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collide with " + collision.gameObject.name + "with powerup set to " + hasPowerup);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+
+
+
+        }
+
+       
+        {
+
+            
+
+        }
+    }
+}
